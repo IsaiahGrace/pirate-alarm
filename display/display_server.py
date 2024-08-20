7ed215ff867fc9d2946f8cf6e82776491ef966d8
Isaiah Grace <isaiah@graces.com>
2024-08-19 21:22:09 -0400

    So close! But something is wrong with the icon bar.

--- display/display_server.py
+++ display/display_server.py
@@ -30,6 +30,7 @@ logger = logging.getLogger(__name__)
 HEIGHT = 240
 WIDTH = 240
 ICON_BAR_HEIGHT = 48
+BACKLIGHT_TIMEOUT = 300
 
 
 class Display:
@@ -66,12 +67,30 @@ class Display:
         self.screen.set_backlight(enabled)
 
 
+class Color:
+    def __init__(self, r, g, b, a):
+        self.r = r
+        self.g = g
+        self.b = b
+        self.a = a
+
+    def __str__(self):
+        r = max(0, min(255, int(self.r)))
+        g = max(0, min(255, int(self.g)))
+        b = max(0, min(255, int(self.b)))
+        a = max(0, min(255, int(self.a)))
+        return f"#{r:02x}{g:02x}{b:02x}{a:02x}"
+
+
 class Compositor:
     def __init__(self, display):
         self.display = display
         self.backlight = threading.Event()
+        self.stop = threading.Event()
         self.active_icons = dict()
-        self.icon_bar_color = (0, 0, 0, 255)
+        self.icon_bar_color = Color(0, 0, 255, 128)
+        self.icon_mask = Image.open(os.path.abspath("../images/icon_bar_mask.png"))
+        self.image = Image.new("RGBA", (WIDTH, HEIGHT))
 
     def __enter__(self):
         self.thread = threading.Thread(target=self.thread_main)
@@ -79,30 +98,73 @@ class Compositor:
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        pass
+        self.stop.set()
+        self.thread.join()
 
     def stopped(self):
-        return display.stopped()
+        if self.display.stopped() or self.stop.is_set():
+            self.stop.set()
+            return True
+        else:
+            return False
 
-    def draw_icon(self, icon):
-        pass
-
-    def clear_icon(self, icon):
-        pass
-
-    def icon_bar_color(self, r, g, b, a):
-        pass
+    def thread_main(self):
+        try:
+            self.backlight_tread()
+        finally:
+            self.stop.set()
 
     def backlight_tread(self):
-        pass
+        last_update_time = time.time()
+        backlight_status = True
+        while not self.stopped():
+            if self.backlight.wait(1.0):
+                if not backlight_status:
+                    self.display.set_backlight(True)
+                last_update_time = time.time()
+                backlight_status = True
+                self.backlight.clear()
+
+            if backlight_status and time.time() - last_update_time >= BACKLIGHT_TIMEOUT:
+                self.display.set_backlight(False)
+                backlight_status = False
+
+    def draw_icon(self, category, symbol):
+        self.active_icons[category] = symbol
+        self.redraw_icons()
+
+    def clear_icon(self, category, symbol):
+        del self.active_icons[category]
+        self.redraw_icons()
+
+    def icon_bar_color(self, r, g, b, a):
+        self.icon_bar_color.r = r
+        self.icon_bar_color.g = g
+        self.icon_bar_color.b = b
+        self.icon_bar_color.a = a
+        self.redraw_icons()
+
+    def redraw_icons(self):
+        icon_bar = Image.new("RGBA", (WIDTH, HEIGHT), str(self.icon_bar_color))
+        for category, symbol in self.active_icons.items():
+            path = os.path.abspath(f"../images/icon_{category}_{symbol}.png")
+            with Image.open(path) as icon_image:
+                icon_bar.paste(icon_image)
+        self.image.paste(icon_bar, mask=self.icon_mask)
+        self.redraw()
 
     def draw_image(self, image_path):
-        logger.debug(f"Display.draw_image('{image_path}')")
-        with Image.open(image_path) as image:
-            if image.width != WIDTH or image.height != HEIGHT:
-                image = image.resize((WIDTH, HEIGHT))
-                logger.debug(f"Image resized from ({image.width},{image.height}) to ({WIDTH},{HEIGHT})")
-            self.display.draw_image(image)
+        logger.debug(f"Compositor.draw_image('{image_path}')")
+        with Image.open(image_path) as new_image:
+            if new_image.width != WIDTH or new_image.height != HEIGHT:
+                new_image = new_image.resize((WIDTH, HEIGHT))
+                logger.debug(f"Image resized from ({new_image.width},{new_image.height}) to ({WIDTH},{HEIGHT})")
+            self.image.paste(new_image)
+        self.redraw()
+
+    def redraw(self):
+        self.backlight.set()
+        self.display.display(self.image)
 
 
 class Server:
@@ -110,14 +172,16 @@ class Server:
         self.compositor = compositor
         self.context = zmq.Context()
         self.socket = self.context.socket(zmq.REP)
-        self.backlight = False
-        self.backlight_timeout = time.time()
 
         self.commands_schema = {
             "clear_icon": (self.cmd_clear_icon, {"icon": str}),
             "draw_icon": (self.cmd_draw_icon, {"icon": str}),
             "icon_bar_color": (self.cmd_icon_bar_color, {"r": int, "g": int, "b": int, "a": int}),
             "draw_image": (self.cmd_draw_image, {"relative_path": str}),
+        }
+        self.icon_types = {
+            "alarm": {"check", "none", "note", "off", "plus"},
+            "wifi": {"connected", "disconnected", "wait"},
         }
 
     def __enter__(self):
@@ -170,21 +234,25 @@ class Server:
             command = cmd["command"]
             self.commands_schema[command][0](cmd)
         except Exception as e:
-            logger.exception("Display server message handler")
+            logger.exception("Server message handler")
             self.respond_error(e)
         else:
             self.respond_ok()
 
     def cmd_draw_icon(self, cmd):
-        self.compositor.draw_icon(cmd["icon"])
+        category, symbol = cmd["icon"].split("_")
+        assert symbol in self.icon_types[category]
+        self.compositor.draw_icon(category, symbol)
 
     def cmd_clear_icon(self, cmd):
-        self.compositor.clear_icon(cmd["icon"])
+        category, symbol = cmd["icon"].split("_")
+        assert symbol in self.icon_types[category]
+        self.compositor.clear_icon(category, symbol)
 
     def cmd_draw_image(self, cmd):
         image = os.path.abspath(cmd["relative_path"])
         assert os.path.exists(image)
-        self.display.draw_image(image)
+        self.compositor.draw_image(image)
 
     def cmd_icon_bar_color(self, cmd):
         self.compositor.icon_bar_color(cmd["r"], cmd["g"], cmd["b"], cmd["a"])
