4581512217a1fe9c473ec3101979e126fc64daec
Isaiah Grace <isaiah@graces.com>
2024-09-03 23:41:40 -0400

    Created a button client library, and logging utility.

--- src/buttonClient.py
+++ src/buttonClient.py
@@ -12,8 +12,23 @@ import zmqNet
 logger = logging.getLogger(__name__)
 
 
+class ButtonClient:
+    def __init__(self):
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.SUB)
+        self.socket.connect(zmqNet.BUTTON_SUB)
+        self.socket.setsockopt_string(zmq.SUBSCRIBE, "")
+
+    def get_button_event(self):
+        msg = self.socket.recv().decode()
+        return msg
+
+
 def log_button_events():
-    pass
+    button_client = ButtonClient()
+    while True:
+        event = button_client.get_button_event()
+        logger.info(event)
 
 
 if __name__ == "__main__":
